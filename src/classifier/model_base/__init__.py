a45c17ff0fb2f98f3b74bef3432ba9ebe869e63d
howie.hu <howie6879@gmail.com>
2021-04-08 13:11:34 +0800

    Updated: README.md

---
+++ src/classifier/model_base/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+"""
+    Created by howie.hu at 2021/4/7.
+    Description：广告分类器模块
+    Changelog: all notable changes to this file will be documented
+"""
---
+++ src/classifier/model_factory.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+"""
+    Created by howie.hu at 2021-04-08.
+    Description：模型预测工厂
+    Changelog: all notable changes to this file will be documented
+"""
+
+
+def model_factory():
+    pass
