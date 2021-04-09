c3b78dd4ac321a6225c28eb9a16ba6cb0e8780d9
howie.hu <howie6879@gmail.com>
2021-04-09 13:28:13 +0800

    Added: ads data

--- scripts/model_predict.py
+++ scripts/model_predict.py
@@ -8,13 +8,36 @@ from src.classifier import model_predict_factory
 from src.config import Config
 from src.databases import MongodbManager
 
-mongo_base = MongodbManager.get_mongo_base(mongodb_config=Config.MONGODB_CONFIG)
 
-coll = mongo_base.get_collection(coll_name="2c_articles")
-for each in coll.find({}):
-    doc_name = each["doc_name"]
-    model_resp = model_predict_factory(
-        model_name="cos", model_path="", input_dict={"text": doc_name, "cos_value": 0.5}
+def cos_pre(text: str, cos_value: int = 0.5):
+    """
+    余弦相似度预测
+    :param text:
+    :type text: str
+    :param cos_value:
+    :type cos_value: int
+    """
+    return model_predict_factory(
+        model_name="cos", model_path="", input_dict={"text": text, "cos_value": 0.5}
     ).to_dict()
-    if model_resp["result"] == 1:
-        print(f"{doc_name} 被识别为广告[{model_resp['probability']}]，链接为：{each['doc_link']}")
+
+
+def test_mongo_doc():
+    """
+    测试数据库文本
+    """
+    mongo_base = MongodbManager.get_mongo_base(mongodb_config=Config.MONGODB_CONFIG)
+    coll = mongo_base.get_collection(coll_name="2c_articles")
+    for each in coll.find({}):
+        doc_name = each["doc_name"]
+        model_resp = cos_pre(text=each["name"])
+        if model_resp["result"] == 1:
+            print(
+                f"{doc_name} 被识别为广告[{model_resp['probability']}]，链接为：{each['doc_link']}"
+            )
+
+
+if __name__ == "__main__":
+    text = "一份来自阿里P9的职场晋升指南”"
+    res = cos_pre(text=text)
+    print(res)
