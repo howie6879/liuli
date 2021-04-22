35633820539ed8fe523bfb2b2b49e3a0f29ab7a1
howie6879 <howie6879@gmail.com>
2021-04-22 22:18:17 +0800

    Added: clean ads

--- scripts/ads_ana.py
+++ scripts/ads_ana.py
@@ -16,35 +16,31 @@ from newspaper import Article
 from textrank4zh import TextRank4Keyword, TextRank4Sentence
 
 from src.config import Config
+from src.databases import MongodbManager
 
 
-def fetch_keyword(url: str):
+def fetch_keyword_list(url: str):
     """
     获取文章简要说明作为样本
     失败就会出现 ['该内容已被发布者删除']
     :param url:
     :return:
     """
-    resp = requests.get(url)
-    print(resp.text)
-    # 进行关键词提取
-    # article = Article(url, language="zh")
-    # article.download()
-    # article.parse()
+    article = Article(url, language="zh")
+    article.download()
+    article.parse()
+    tr4w = TextRank4Keyword()
+    tr4w.analyze(
+        text=article.text, lower=True, window=2, vertex_source="words_no_stop_words"
+    )
+    keyword_list = []
+    for item in tr4w.get_keywords(20, word_min_len=2):
+        keyword_list.append(item.word)
 
-    # print(article.html)
-    #
-    # tr4s = TextRank4Sentence()
-    # tr4s.analyze(text=text, lower=True, source='all_filters')
-    #
-    # print()
-    # print('摘要：')
-    # for item in tr4s.get_key_sentences(num=3):
-    #     print(item.index, item.weight, item.sentence)
-    return ""
+    return keyword_list
 
 
-def gen_keyword_ads(url: str):
+def gen_keyword_ads():
     """
     广告文本提取关键词
     :param url:
@@ -52,9 +48,17 @@ def gen_keyword_ads(url: str):
     """
 
     ads_path = "../.files/datasets/ads.csv"
+    clean_ads_path = "../.files/datasets/clean_ads.csv"
     df = pd.read_csv(ads_path)
+    clean_df = pd.read_csv(clean_ads_path)
 
-    res = []
+    ads_res = []
+    clean_ads_res = []
+
+    for each in clean_df.values.tolist():
+        clean_ads_res.append(
+            {"title": each[0], "keywords": each[1],}
+        )
 
     for each in df.values.tolist():
         title, url, is_process = each[0], each[1], each[2]
@@ -63,15 +67,24 @@ def gen_keyword_ads(url: str):
             "url": url,
             "is_process": is_process,
         }
-        res.append(cur_data)
         if is_process == 0:
             # 进行提取，成功就改值为1
-            keyword = fetch_keyword(url)
-            # 判断是否被删除
-            if "该内容已被发布者删除" in keyword:
-                print(f"{title} {url} 需要重新收集有效链接")
-            else:
-                print(keyword)
+            keyword_list = fetch_keyword_list(url)
+            if keyword_list:
+                # 判断是否被删除
+                if "发布者" in keyword_list and "删除" in keyword_list:
+                    cur_data["is_process"] = "0"
+                    print(f"{title} {url} 需要重新收集有效链接")
+                else:
+                    cur_data["is_process"] = "1"
+                    clean_ads_res.append(
+                        {"title": title, "keywords": ";".join(keyword_list)}
+                    )
+        ads_res.append(cur_data)
+
+    # 持久化
+    pd.DataFrame(ads_res).to_csv(ads_path, index=False)
+    pd.DataFrame(clean_ads_res).to_csv(clean_ads_path, index=False)
 
 
 def csv2txt(target_path: str = ""):
@@ -98,5 +111,7 @@ def csv2txt(target_path: str = ""):
 
 if __name__ == "__main__":
     # csv2txt()
-    keyword = fetch_keyword("https://mp.weixin.qq.com/s/VhoRl9ispyAJCFnf9244Uw")
-    print(keyword)
+    # url = "https://www.ershicimi.com/p/f4b00ac47a2b2fa2ce50719ed2787963"
+    # keyword_list = fetch_keyword_list(url)
+    # print(keyword_list)
+    gen_keyword_ads()
