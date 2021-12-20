3253b794c654713bdac33e447cdf1f5ee0ea61aa
howie.hu <1757297452@qq.com>
2021-12-20 18:04:14 +0800

    Update: add wechat collector by playwright

--- src/collector/process.py
+++ src/collector/process.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """
     Created by howie.hu at 2021/04/29.
-    Description: 对采集器获取的文章进行特殊出创出，如关键字提取
+    Description: 对采集器获取的文章进行特殊处理，如关键字提取
     Changelog: all notable changes to this file will be documented
 """
