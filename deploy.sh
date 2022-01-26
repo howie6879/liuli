23eed9ad73cafbb3700fe4589751221ffc487e31
howie6879 <howie6879@gmail.com>
2022-01-26 15:05:59 +0800

    Update: deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ docker build --no-cache=true -t liuliio/schedule:v0.1.5 -f schedule.Dockerfile .
 # 打包使用缓存
 docker build --no-cache=false -t liuliio/schedule:v0.1.5 -f schedule.Dockerfile .
 # 运行
-docker run -d -it --restart=always -v $PWD/pro.env:/data/code/pro.env --name liuli_schedule liuliio/schedule:v0.1.5
+docker run -d -it --restart=always -v $PWD/pro.env:/data/code/pro.env -v $PWD/liuli_config:/data/code/liuli_config --name liuli_schedule liuliio/schedule:v0.1.5
 # 上传
 docker push liuliio/schedule:v0.1.5
 
