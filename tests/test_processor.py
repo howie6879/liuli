8121900e6786e728a2bd5ca016dd592cd58bb4e8
howie6879 <howie6879@gmail.com>
2022-02-10 22:48:51 +0800

    Update test case

--- tests/test_processor.py
+++ tests/test_processor.py
@@ -6,6 +6,7 @@
 """
 
 from src.common.remote import get_html_by_requests
+from src.config import Config
 from src.processor.text_utils import extract_chapters
 
 
@@ -15,6 +16,8 @@ def test_extract_chapters():
     """
     chapter_url = "https://book.qidian.com/info/1010868264/#Catalog"
     # chapter_url = "https://www.iwurexs.com/read/309300/"
-    resp_text = get_html_by_requests(chapter_url)
+    resp_text = get_html_by_requests(
+        chapter_url, headers={"User-Agent": Config.SPIDER_UA}
+    )
     chapters_res = extract_chapters(chapter_url=chapter_url, html=resp_text)
     assert len(chapters_res) > 1
