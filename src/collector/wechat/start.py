7028325a08c0230e82bbdac304c4291bb93fc653
howie.hu <1757297452@qq.com>
2022-04-07 19:51:06 +0800

    Update

--- src/collector/wechat/start.py
+++ src/collector/wechat/start.py
@@ -3,7 +3,6 @@
     Description: 搜狗微信爬虫启动函数
     Changelog: all notable changes to this file will be documented
 """
-from src.collector.wechat.sg_ruia_start import run as ruia_run
 
 
 def run(collect_config: dict):
@@ -14,6 +13,8 @@ def run(collect_config: dict):
     """
     spider_type = collect_config.get("spider_type", "ruia")
     if spider_type == "ruia" or spider_type == "sg_ruia":
+        from src.collector.wechat.sg_ruia_start import run as ruia_run
+
         run_func = ruia_run
     elif spider_type == "playwright" or spider_type == "sg_playwright":
         from src.collector.wechat.sg_playwright_start import run as playwright_run
