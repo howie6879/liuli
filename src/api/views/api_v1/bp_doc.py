9ccd74c00106b26ca0e1bb4832ddd29eca7d4db7
howie6879 <howie6879@gmail.com>
2022-05-24 14:17:33 +0800

    Update: add rss_list api(bp_doc)

--- src/api/views/api_v1/bp_doc.py
+++ src/api/views/api_v1/bp_doc.py
@@ -4,6 +4,8 @@
     Changelog: all notable changes to this file will be documented
 """
 
+from urllib.parse import urljoin
+
 from flask import Blueprint, current_app, request
 
 from src.api.common import (
@@ -14,21 +16,76 @@ from src.api.common import (
     jwt_required,
     response_handle,
 )
+from src.config import Config
 from src.databases import MongodbBase
-from src.utils import LOGGER
+from src.utils import LOGGER, get_ip, ts_to_str_date
 
 bp_doc = Blueprint("doc", __name__, url_prefix="/doc")
 
 
-@bp_doc.route("/doc_source_list", methods=["POST"], strict_slashes=False)
+@bp_doc.route("/rss_list", methods=["POST"], strict_slashes=False)
 @jwt_required()
-def token_valid():
+def rss_list():
+    """获取用户下所有rss链接地址
+    eg:
+    {
+        "username": "liuli",
+        "doc_source": "",
+    }
+    Returns:
+        Response: 响应类
+    """
+    mongodb_base: MongodbBase = current_app.config["mongodb_base"]
+    app_logger: LOGGER = current_app.config["app_logger"]
+    app_config: Config = current_app.config["app_config"]
+    coll = mongodb_base.get_collection(coll_name="liuli_rss")
+    # 获取基础数据
+    post_data: dict = request.json
+    doc_source = post_data.get("doc_source", "")
+    filter_dict = {"doc_source": doc_source} if doc_source else {}
+    return_dict = {"_id": 0, "doc_source": 1, "doc_source_name": 1, "updated_at": 1}
+    domain: str = app_config.DOMAIN or f"http://{get_ip()}:{Config.HTTP_PORT}"
+
+    try:
+        cursor = coll.find(filter_dict, return_dict).sort("updated_at", 1)
+        rss_dict = []
+        for document in cursor:
+            updated_at = document["updated_at"]
+            doc_source = document["doc_source"]
+            doc_source_name = document["doc_source_name"]
+            rss_dict.append(
+                {
+                    **document,
+                    **{
+                        "updated_at": ts_to_str_date(updated_at),
+                        "rss_url": urljoin(
+                            domain, f"rss/{doc_source}/{doc_source_name}"
+                        ),
+                    },
+                }
+            )
+        # 查询成功
+        result = {
+            ResponseField.DATA: rss_dict,
+            ResponseField.MESSAGE: ResponseReply.SUCCESS,
+            ResponseField.STATUS: ResponseCode.SUCCESS,
+        }
+    except Exception as e:
+        result = UniResponse.DB_ERR
+        err_info = f"query doc RSS failed! DB response info -> {e}"
+        app_logger.error(err_info)
+
+    return response_handle(request=request, dict_value=result)
+
+
+@bp_doc.route("/source_list", methods=["POST"], strict_slashes=False)
+@jwt_required()
+def source_list():
     """获取所有文档源统计信息
     eg:
     {
         "username": "liuli"
     }
-    doc_source 为空表示查询所有源信息
     Returns:
         Response: 响应类
     """
