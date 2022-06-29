fd9b658522e15dcb4b1b639dc3c66f3a433b3fb4
howie6879 <howie6879@gmail.com>
2022-06-29 11:56:57 +0800

    Update docker&nginx config for web

--- liuli_web/deploy/deploy.sh
+++ liuli_web/deploy/deploy.sh
@@ -1,5 +1,16 @@
 # Web项目镜像构建
-docker buildx build --no-cache=false --platform linux/amd64 -t docker.zfty.work/brandark/brandark-web:v0.1.0-dev -f Dockerfile .
-docker build --no-cache=false -t docker.zfty.work/brandark/brandark-web:v0.1.0-dev -f Dockerfile .
+docker buildx build --no-cache=false --platform linux/amd64 -t liuliio/web-amd64:v0.1.0 -f Dockerfile .
+docker buildx build --no-cache=false --platform linux/arm64 -t liuliio/web-arm64:v0.1.0 -f Dockerfile .
+docker buildx build --no-cache=false --platform linux/arm/v7 -t liuliio/web-armv7:v0.1.0 -f Dockerfile .
+docker buildx build --no-cache=false --platform linux/arm/v8 -t liuliio/web-armv8:v0.1.0 -f Dockerfile .
 
-docker run -p 8080:80 -e BA_API=http://192.168.1.16:18432 docker.zfty.work/brandark/brandark-web:v0.1.0-dev
+docker push liuliio/web-amd64:v0.1.0
+docker push liuliio/web-armv7:v0.1.0
+docker push liuliio/web-armv8:v0.1.0
+docker push liuliio/web-arm64:v0.1.0
+
+docker manifest rm liuliio/web:v0.1.0
+docker manifest create liuliio/web:v0.1.0 liuliio/web-amd64:v0.1.0 liuliio/web-armv7:v0.1.0 liuliio/web-armv8:v0.1.0 liuliio/web-arm64:v0.1.0
+docker manifest push liuliio/web:v0.1.0
+
+docker run -p 8080:80 -e LL_API=http://liuli_api:8765 liuliio/web:v0.1.0
--- liuli_web/deploy/nginx_start.sh
+++ liuli_web/deploy/nginx_start.sh
@@ -2,20 +2,18 @@
 
 file='/etc/nginx/conf.d/default.conf'
 
-if [ ! $BA_API ]; then
-    echo "Backend API is empty! please set an environment variable named {BA_API}."
+if [ ! $LL_API ]; then
+    echo "Backend API is empty! please set an environment variable named {LL_API}."
     exit
 else
-    echo "Backend API is: "$BA_API
-    BA_API=${BA_API//\//\\\/}
+    echo "Backend API is: "$LL_API
+    LL_API=${LL_API//\//\\\/}
     if [ "$(uname)" == "Darwin" ];
     then
-        # command="sed -i '' 's/proxy_pass localhost/proxy_pass '$BA_API'/g' "$file
-        # echo $command
-        sed -i '' 's/proxy_pass localhost/proxy_pass '$BA_API'/g' $file
+        sed -i '' 's/proxy_pass localhost/proxy_pass '$LL_API'/g' $file
     elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ];
     then
-        sed -i 's/proxy_pass localhost/proxy_pass '$BA_API'/g' $file
+        sed -i 's/proxy_pass localhost/proxy_pass '$LL_API'/g' $file
     fi
     echo "Start Nginx..."
     nginx -g "daemon off;"
