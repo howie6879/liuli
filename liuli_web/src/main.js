d613588f9e09483687cf26dd5b52d94308fee798
didnhdj <didnhdj2@gmail.com>
2023-02-05 21:16:07 +0800

    Liuli web : add some package & styles & pic

--- liuli_web/src/main.js
+++ liuli_web/src/main.js
@@ -1,18 +1,36 @@
 import { createApp } from 'vue';
 
 // 导入 CSS 库
-import '../public/static/css/global.css';
+// import '/public/static/css/global.css';
 import '../public/static/css/pico.min.css';
+// import '@picocss/pico';
+import '../public/static/css/global.css';
+
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
 
 // 导入自定义模块
 import api from './api/index';
 import App from './App.vue';
 import router from './router/index';
 import pinia from './store/index';
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+// import install  from './assets/icons/index'
 
+import 'virtual:svg-icons-register'
 const app = createApp(App);
+
+import svgIcon from '@/components/svgIcon'
+app.component('SvgIcon', svgIcon)
 // 挂载 router
 app.use(router);
+// 注册el-plus组件
+app.use(ElementPlus)
+// 注册el -icons
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+  }
 // 挂载 pinia
 app.use(pinia);
 // 挂载 axios 全局对象
--- liuli_web/vite.config.js
+++ liuli_web/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
+const path = require('path');
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -10,8 +11,35 @@ export default defineConfig({
           isCustomElement: (tag) => tag === 'hgroup'
         }
       }
-    })
+    }),
+    createSvgIconsPlugin({
+      // 指定需要缓存的图标文件夹
+      iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
+      // 指定symbolId格式
+      symbolId: 'icon-[dir]-[name]',
+
+      /**
+       * 自定义插入位置
+       * @default: body-last
+       */
+      inject: 'body-last' | 'body-first',
+
+      /**
+       * custom dom id
+       * @default: __svg__icons__dom__
+       */
+      customDomId: '__svg__icons__dom__',
+    }),
   ],
+  
+  resolve: {
+    // 配置路径别名
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+    // 导入简化后缀名
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue']
+  },
   server: {
     host: '0.0.0.0',
     port: 8080,
