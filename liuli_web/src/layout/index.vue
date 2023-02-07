f7c1d6ceb37b085139bdc368ad07af5557cc78d3
didnhdj2 <didnhdj2@gmail.com>
2023-02-07 23:29:10 +0800

    Update: remove some package & rename files

--- liuli_web/src/layout/index.vue
+++ liuli_web/src/layout/index.vue
@@ -1,50 +1,50 @@
 <template>
-    <div class="main">
-        <el-container class="main-content">
-            <el-collapse-transition>
-                <el-aside :width="isCollapse ? '80px' : '210px'">
-                    <nav-menu :collapse="isCollapse" />
-                </el-aside>
-            </el-collapse-transition>
-            <el-container class="page">
-                <el-header class="page-header">
-                    <nav-header @foldChange="foldChange" :isCollapse="isCollapse" />
-                </el-header>
-                <el-main class="page-content">
-                    <!-- 页面视图 -->
-                    <div class=" plr-10  page-content-main">
-                        <router-view></router-view>
-                    </div>
-                </el-main>
-            </el-container>
-        </el-container>
-    </div>
+  <div class="main">
+    <el-container class="main-content">
+      <el-collapse-transition>
+        <el-aside :width="isCollapse ? '80px' : '210px'">
+          <nav-menu :collapse="isCollapse" />
+        </el-aside>
+      </el-collapse-transition>
+      <el-container class="page">
+        <el-header class="page-header">
+          <nav-header @foldChange="foldChange" :isCollapse="isCollapse" />
+        </el-header>
+        <el-main class="page-content">
+          <!-- 页面视图 -->
+          <div class="plr-10 page-content-main">
+            <router-view></router-view>
+          </div>
+        </el-main>
+      </el-container>
+    </el-container>
+  </div>
 </template>
 
 <script setup>
-import { provide, ref } from 'vue'
-import navHeader from "./components/navHeader";
-import navMenu from "./components/navMenu";
-const isCollapse = ref(false)
-provide('isCollapse', isCollapse)
+import { provide, ref } from 'vue';
+import navHeader from './components/navHeader';
+import navMenu from './components/navMenu';
+const isCollapse = ref(false);
+provide('isCollapse', isCollapse);
 function foldChange() {
-    isCollapse.value = !isCollapse.value
+  isCollapse.value = !isCollapse.value;
 }
 </script>
 <style scoped lang="scss">
 .main {
-    min-height: 100vh;
+  min-height: 100vh;
 
-    .header {
-        background-color: #fff;
+  .header {
+    background-color: #fff;
+  }
+
+  .page-content {
+    padding: 0;
+
+    &-main {
+      padding: 20px;
     }
-
-    .page-content {
-        padding: 0;
-
-        &-main {
-            padding: 20px;
-        }
-    }
+  }
 }
 </style>
